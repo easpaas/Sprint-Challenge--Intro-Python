1e2a568f766a330c681b3e17980528262c1dcd27
evan <aspaasevan@gmail.com>
2020-06-05 09:30:37 -0700

    oop1-test passes 6tests

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -1,5 +1,37 @@
 # Write classes for the following class hierarchy:
+
+
 #
+## Base Class - vehicle
+#
+class Vehicle:
+  pass
+
+
+# Inherit Vehicle class
+class FlightVehicle(Vehicle): 
+  pass
+
+# Inherit FlightVehicle class 
+class Airplane(FlightVehicle):
+  pass
+
+# Inherit FlightVehicle class 
+class Starship(FlightVehicle):
+  pass
+
+# Inherit Vehicle class
+class GroundVehicle(Vehicle):
+  pass
+
+# Inherit GroundVehicle class 
+class Car(GroundVehicle):
+  pass
+
+# Inherit GroundVehicle class
+class Motorcycle(GroundVehicle):
+  pass
+
 #  [Vehicle]->[FlightVehicle]->[Starship]
 #      |                |
 #      v                v
