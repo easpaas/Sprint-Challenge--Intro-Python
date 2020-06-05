090b556cc0c376110e7c36c172db282e0da44daa
evan <aspaasevan@gmail.com>
2020-06-05 11:34:20 -0700

    City instance created from csv

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,5 +1,16 @@
+import csv
+
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+class City: 
+  def __init__(self, name, lat, lon):
+    self.name = name 
+    self.lat = lat 
+    self.lon = lon
+
+  def __str__(self):
+    return f"<City: {self.name}, Lat: {self.lat}, Lon: {self.lon}>"
+
 
 
 # We have a collection of US cities with population over 750,000 stored in the
@@ -17,11 +28,19 @@
 cities = []
 
 def cityreader(cities=[]):
-  # TODO Implement the functionality to read from the 'cities.csv' file
+  # TODO Implement the 
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
-    return cities
+    with open('cities.csv', newline='') as fp:
+      reader = csv.reader(fp) # functionality to read from the 'cities.csv' file
+      # cities = [row for row in reader]
+      for record in reader:
+          # TODO exclude the first record from being imported into a City instance
+          # Each record (city) in the csv file is imported into a City instance
+          cities.append(City(record[0], record[3], record[4]))
+          print(cities)
+
+      return cities
 
 cityreader(cities)
 
